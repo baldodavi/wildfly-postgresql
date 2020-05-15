c3d3e5ca19b3835df710e75819563c8d72b8b65d
baldodavi <baldodavi@gmail.com>
2020-05-15 18:12:57 +0200

    sdfsd

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 TAG=0.9.14
-docker build -f redeccg-production-wildfly-db-drivers.dockerfile -t redeccg-production-wildfly-db-drivers:$TAG .
+docker build -f redeccg-production-wildfly-db-drivers-debian.dockerfile -t redeccg-production-wildfly-db-drivers:$TAG .
 docker tag redeccg-production-wildfly-db-drivers:$TAG david-baldo.com/redeccg-production-wildfly-db-drivers:$TAG
 docker push david-baldo.com/redeccg-production-wildfly-db-drivers:$TAG
