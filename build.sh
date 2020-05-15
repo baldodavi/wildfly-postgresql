1951caf0f8be0b70b9d0d84506c3223f1f44ddef
baldodavi <baldodavi@gmail.com>
2020-05-15 18:18:48 +0200

    asdas

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 TAG=0.9.14
 NAME=redeccg-production-wildfly-db-drivers-debian
 docker build -f $NAME.dockerfile -t $NAME:$TAG .
-docker tag NAME:$TAG david-baldo.com/$NAME:$TAG
+docker tag $NAME:$TAG david-baldo.com/$NAME:$TAG
 docker push david-baldo.com/$NAME:$TAG
