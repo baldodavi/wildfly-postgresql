326a2f5f4a8a1600a7388074771c497c5ffb95ab
baldodavi <baldodavi@gmail.com>
2020-05-15 15:25:41 +0200

    qweqwe

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-env TAG = 0.9.12
+env TAG = 0.9.13
 docker build -f redeccg-production-wildfly-db-drivers.dockerfile -t redeccg-production-wildfly-db-drivers:$TAG .
 docker tag redeccg-production-wildfly-db-drivers:$TAG
